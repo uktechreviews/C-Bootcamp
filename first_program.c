7f53fe5d683818d32661bd5c238ef90a21d98bc6
uktechreviews <spencerorgan1@me.com>
2023-12-21 20:26:03 +0000

    first program with loops

---
+++ first_program.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = a * 10;
+    printf ("%d \n", a);
+    printf ("%d \n", b);
+
+    char name[50] = "Spencer";
+    printf ("%s \n", name);
+
+    int count;
+
+    for (count = 0; 
+    count <100; 
+    count ++)
+
+    {
+        printf("My name is %s\n", name);
+    }
+
+    return 0;
+}
+
